c5d463e063325a7dcca444c580d758a8720262a8
Diego Banovaz <diego@strive.school>
2019-09-03 11:20:28 +0200

    Add project files.

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+    }
+
+    class BinaryTree //Not B Tree
+    {
+        //startingNode is NOT the root of the Tree
+        public TreeNode Insert(TreeNode startingNode, int value)
+        {
+            if (startingNode == null)
+            {
+                startingNode = new TreeNode();
+                startingNode.value = value;
+                //Console.WriteLine("Creating a leaf for " + value);
+            }
+            else
+            {
+                if (value < startingNode.value)
+                {
+                  //  Console.WriteLine(value + " < " + startingNode.value + " => going to the left");
+                    startingNode.left = Insert(startingNode.left, value);
+                }
+                else
+                {
+                    //Console.WriteLine(value + " >= " + startingNode.value + " => going to the right");
+                    startingNode.right = Insert(startingNode.right, value);
+                }
+            }
+
+            return startingNode;
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binaryTree = new BinaryTree();
+            var root = binaryTree.Insert(null, 1);
+
+            var randomGenerator = new Random();
+           
+            var frameworkAlternative = new SortedDictionary<int, string>();
+            for(var i = 0; i < 1024*1024*10; i++)
+            {
+                frameworkAlternative.Add(i, "whatever string " + randomGenerator.Next(1000000));
+                //binaryTree.Insert(root, randomGenerator.Next(1000000));
+            }
+
+            var sw = Stopwatch.StartNew();
+            var res = frameworkAlternative[12409];
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            //binaryTree.Insert(root, 10);
+            //binaryTree.Insert(root, 12);
+            //binaryTree.Insert(root, 8);
+            //binaryTree.Insert(root, 5);
+            //binaryTree.Insert(root, 6);
+            //binaryTree.Insert(root, 4);
+            //binaryTree.Insert(root, 7);
+            //binaryTree.Insert(root, 10);
+            //binaryTree.Insert(root, 10);
+            //binaryTree.Insert(i, 11);
+            //binaryTree.Insert(i, 100);
+            //binaryTree.Insert(i, 1100);
+        }
+    }
+
+
+
+}
+
